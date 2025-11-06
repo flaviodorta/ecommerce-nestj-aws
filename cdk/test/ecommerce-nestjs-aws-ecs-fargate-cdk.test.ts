a7be5094e70f32010e2efeb85564d120d842b19f
Flávio Dorta <dorta.dev@gmail.com>
2025-11-05 21:57:21 -0300

    init project

---
+++ cdk/test/ecommerce-nestjs-aws-ecs-fargate-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcommerceNestjsAwsEcsFargateCdk from '../lib/ecommerce-nestjs-aws-ecs-fargate-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecommerce-nestjs-aws-ecs-fargate-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcommerceNestjsAwsEcsFargateCdk.EcommerceNestjsAwsEcsFargateCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
