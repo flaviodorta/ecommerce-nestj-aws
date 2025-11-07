2ed0cc527fbaa13edc552887e7c2b7f68abb8b1a
Flávio Dorta <dorta.dev@gmail.com>
2025-11-06 23:23:17 -0300

    env, config ecr stack

--- cdk/bin/ecommerce-nestjs-aws-ecs-fargate-cdk.ts
+++ cdk/bin/ecommerce-nestjs-aws-ecs-fargate-cdk.ts
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { EcommerceNestjsAwsEcsFargateCdkStack } from '../lib/ecommerce-nestjs-aws-ecs-fargate-cdk-stack';
+import { EcrStack } from '../lib/ecr-stack';
 
 const app = new cdk.App();
-new EcommerceNestjsAwsEcsFargateCdkStack(app, 'EcommerceNestjsAwsEcsFargateCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const tagsInfra = {
+  cost: 'EcommerceInfra',
+  team: 'SiecolaCode',
+};
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const env: cdk.Environment = {
+  account: '500294431003',
+  region: 'us-east-1',
+};
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const newStack = new EcrStack(app, 'Ecr', {
+  tags: tagsInfra,
+  env: env,
 });
